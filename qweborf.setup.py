88379c0137aa923141dc862b53d26c86d078b1c1
pepa65 <solusos@passchier.net>
2017-03-27 14:23:36 +0700

    Release 0.13.5d Improve install and release mechanism

--- qweborf.setup.py
+++ qweborf.setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    version='0.13.5c',
+    version='0.13.5d',
     name='qweborf',
     author="Salvo 'LtWorf' Tomaselli",
     author_email='tiposchi@tiscali.it',
